e0555d1ad9521c0fd562241cd129057a1912024b
rvpacheco <124650306+rvpacheco@users.noreply.github.com>
2023-05-24 22:16:37 +0000

    everything perfect

--- src/app.py
+++ src/app.py
@@ -1,23 +1,16 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, People, Planets, Favorites
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import create_access_token, JWTManager
-from flask_jwt_extended import jwt_required
-#from models import Person
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-app.config["JWT_SECRET_KEY"]=os.getenv("FLASK_APP_KEY")
+app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_APP_KEY")
 jwt = JWTManager(app)
 
 db_url = os.getenv("DATABASE_URL")
@@ -32,149 +25,102 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
-@app.route('/user', methods=['POST'])
-def user_create():
-    data = request.get_json()
-    print(data)
-    new_user=User.query.filter_by(email=data["email"]).first()
-    if(new_user is not None):
-        return jsonify({
-            "msg":"Email registrado"
-        }), 400
-    new_user = User(email=data["email"], password=data["password"], is_active=True)
-    db.session.add(new_user)
-    db.session.commit()
-    print("_____ Objeto de base de datos ______")
-    print(new_user)
-    print("_____ Diccionario ______")
-    print(new_user.serialize())
-    return "ok"
-
-@app.route("/signup", methods=["POST"])
-def signup():
-    data = request.get_json()
-    print(data)
-    new_user = User.query.filter_by(email=data["email"]).first()
-    if (new_user is not None):
-        return jsonify({
-            "msg":"Email registrado"
-        }),400
-    secure_password = bcrypt.generate_password_hash(
-        data["password"],rounds = None).decode("utf-8")
-    new_user = User(email = data["email"],
-                    password = secure_password, is_active=True)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.serialize()), 201
-
-@app.route("/login", methods=["POST"])
-def user_login():
-    user_email = request.json.get("email")
-    user_password = request.json.get("password")
-    # buscar el usuario por el correo
-    user = User.query.filter_by(email=user_email).first()
-    if user is None:
-        return jsonify({"message": "User no found"}), 401
-    
-    #verificar la clave
-    if not bcrypt.check_password_hash(user.password, user_password):
-        return jsonify({"message": "Wrong password"}),401
-    #generar el token
-    acces_token = create_access_token(identify=user.id)
-    #retornar el token
-    return jsonify({"accessToken": acces_token})
-
-@app.route("/helloprotected",methods=['GET'])
-@jwt_required()
-def hello_protected_get():
-    user_id= get_jwt_identity()
-    return jsonify({
-        "userId": user_id,
-        "message": "Hello protected routed"
-    })
-    
+@app.route('/users', methods=['GET'])
+def users_get():
+    users = User.query.all()
+    users = [user.serialize() for user in users]
+    return jsonify(users)
 
 
-
-
-@app.route("/user/<int:user_id>", methods=["GET"])
-def user_get(user_id):
-    user=User.query.get(user_id)
-    if(user is None):
-        return jsonify({"msg":"Usuario no registrado"}), 404
-    
-    return jsonify(user.serialize())
-
-@app.route("/user/<int:user_id>/favorites", methods=["GET"])
-def user_favorites_get(user_id):
-    favorites=Favorites.query.filter_by(user_id=user_id).all()
-    favorites=list(map(lambda fav: fav.serialize(),favorites))
+@app.route('/users/favorites', methods=['GET'])
+def user_favorites_get():
+    user_id = request.headers.get('user_id')
+    favorites = Favorites.query.filter_by(user_id=user_id).all()
+    favorites = [favorite.serialize() for favorite in favorites]
     return jsonify(favorites)
 
-@app.route("/favorite/<element>/<int:element_id>", methods=["POST"])
-def favorite_create(element, element_id):
-    user_id=request.get_json()["userId"]
-    new_favorite=Favorites(type=element, element_id=element_id, user_id=user_id)
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def favorite_create_planet(planet_id):
+    user_id = request.headers.get('user_id')
+    new_favorite = Favorites(type='planet', element_id=planet_id, user_id=user_id)
     db.session.add(new_favorite)
     db.session.commit()
-    return jsonify({"msg":"Favorite created"}), 201
+    return jsonify({"msg": "Favorite planet created"}), 201
 
-@app.route("/favorite/<element>/<int:element_id>", methods=["DELETE"])
-def favorite_delete(element, element_id):
-    user_id=request.get_json()["userId"]
-    favorite=Favorites.query.filter_by(type=element, element_id=element_id, user_id=user_id).first()
-    if(favorite is None):
-        return jsonify({"msg":"Favorite not found"}), 404
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def favorite_create_people(people_id):
+    user_id = request.headers.get('user_id')
+    new_favorite = Favorites(type='people', element_id=people_id, user_id=user_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify({"msg": "Favorite people created"}), 201
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def favorite_delete_planet(planet_id):
+    user_id = request.headers.get('user_id')
+    favorite = Favorites.query.filter_by(type='planet', element_id=planet_id, user_id=user_id).first()
+    if favorite is None:
+        return jsonify({"msg": "Favorite planet not found"}), 404
     db.session.delete(favorite)
     db.session.commit()
-    return jsonify({"msg":"Favorite deleted"}), 200
+    return jsonify({"msg": "Favorite planet deleted"}), 200
 
-@app.route("/people", methods=["GET"])
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def favorite_delete_people(people_id):
+    user_id = request.headers.get('user_id')
+    favorite = Favorites.query.filter_by(type='people', element_id=people_id, user_id=user_id).first()
+    if favorite is None:
+        return jsonify({"msg": "Favorite people not found"}), 404
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({"msg": "Favorite people deleted"}), 200
+
+
+@app.route('/people', methods=['GET'])
 def people_get():
-    people=People.query.all()
-    people=list(map(lambda person: person.serialize(), people))
+    people = People.query.all()
+    people = [person.serialize()
+     for person in people]
     return jsonify(people)
 
-@app.route("/people/<int:people_id>", methods=["GET"])
-def people_getId(people_id):
-    people=People.query.get(people_id)
-    if (people is None):
-        return jsonify({"msg":"People no registrado"}), 404
-    
-    return jsonify(people.serialize())
 
-@app.route("/planets", methods=["GET"])
+@app.route('/people/<int:people_id>', methods=['GET'])
+def person_get(people_id):
+    person = People.query.get(people_id)
+    if person is None:
+        return jsonify({"msg": "Person not found"}), 404
+    return jsonify(person.serialize())
+
+
+@app.route('/planets', methods=['GET'])
 def planets_get():
-    planets=Planets.query.all()
-    planets=list(map(lambda planet: planet.serialize(), planets))
+    planets = Planets.query.all()
+    planets = [planet.serialize() for planet in planets]
     return jsonify(planets)
 
-@app.route("/planets/<int:planets_id>", methods=["GET"])
-def planets_getId(planets_id):
-    planets=Planets.query.get(planets_id)
-    if (planets is None):
-        return jsonify({"msg":"planets no registrado"}), 404
-    
-    return jsonify(planets.serialize())
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def planet_get(planet_id):
+    planet = Planets.query.get(planet_id)
+    if planet is None:
+        return jsonify({"msg": "Planet not found"}), 404
+    return jsonify(planet.serialize())
+
 
 
 # this only runs if `$ python src/app.py` is executed
